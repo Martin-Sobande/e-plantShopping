f151f7e4de2444f154f38b25be4a1148ae0740fb
Martin-Sobande <martinsobande@outlook.com>
2026-04-25 07:17:03 -0400

    update of CartItem.jsx base code

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        parseFloat(item.cost.substring(1))
+        total += item.cost * item.quantity;
+      });
+      return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    if (item.name.quantity > 0) {
+        dispatch(removeItem(item.name));
+      }
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0; 
+    totalCost = item.name.quantity * item.name.cost; 
   };
 
   return (
